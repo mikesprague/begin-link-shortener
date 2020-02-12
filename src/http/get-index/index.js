8a3dc15dd67d7ed55e43e198b97e4bf596ea6354
Michael Sprague <ms388@cornell.edu>
2020-02-12 11:54:24 -0500

    :recycle: Refactor with short link lookup

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,5 +1,6 @@
-// const begin = require('@architect/functions');
-const templates = require('@architect/shared/templates.js');
+const data = require('@begin/data');
+const begin = require('@architect/functions');
+const templates = require('@architect/shared/templates');
 
 const html = templates.getIndexHtml();
 
@@ -7,18 +8,33 @@ exports.handler = async function http(req) {
   const { pathParameters } = req;
   if (pathParameters) {
     const { proxy: shortId } = pathParameters;
-    console.log('pathParameters: ', pathParameters);
     console.log('shortId: ', shortId);
-    if (shortId && shortId.length === 7) {
-      console.log(`it's a short link!`);
+    if (shortId && shortId.length === 8) {
+      console.log("it's a short link!");
+      const shortLink = await data.get({
+        table: 'links',
+        key: shortId,
+      });
+      console.log('shortLink: ', shortLink);
+      if (shortLink) {
+        return {
+          statusCode: 302,
+          headers: { location: shortLink.original_url },
+        };
+      }
+      return {
+        statusCode: 404,
+        headers: { 'content-type': 'text/html; charset=utf8' },
+        body: 'Short link not found',
+      };
     }
   }
 
   return {
     headers: {
       'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      // 'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
     },
-    body: html
-  }
-}
+    body: html,
+  };
+};
