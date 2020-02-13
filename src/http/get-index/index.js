c90dfa2fac0b0c0401fa783472c90c38db86bca2
Michael Sprague <ms388@cornell.edu>
2020-02-13 08:19:29 -0500

    :bug: Add bugsnag

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,23 +1,29 @@
+const bugsnag = require('@bugsnag/js');
 const data = require('@begin/data');
 const templates = require('@architect/shared/templates');
 const helpers = require('@architect/shared/helpers');
 
 const html = templates.getIndexHtml();
 
+const bugsnagClient = bugsnag(process.env.BUGSNAG_KEY);
+
 exports.handler = async function http(req) {
   const { pathParameters } = req;
   if (pathParameters) {
-    const { proxy: shortId } = pathParameters;
-    if (shortId && shortId.length === 8) {
-      const shortLink = await data.get({
-        table: 'links',
-        key: shortId.replace('/', ''),
-      });
-      if (shortLink) {
-        console.log(shortLink);
-        return helpers.returnShortLink(shortLink);
+    try {
+      const { proxy: shortId } = pathParameters;
+      if (shortId && shortId.length === 8) {
+        const shortLink = await data.get({
+          table: 'links',
+          key: shortId.replace('/', ''),
+        });
+        if (shortLink) {
+          return helpers.returnShortLink(shortLink);
+        }
+        return helpers.returnNotFound();
       }
-      return helpers.returnNotFound();
+    } catch (error) {
+      bugsnagClient.notify(error);
     }
   }
   return helpers.returnPage(html);
