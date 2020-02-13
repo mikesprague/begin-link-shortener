8b146f791738002292697e79399f5b5a05f0a645
Michael Sprague <ms388@cornell.edu>
2020-02-13 08:27:08 -0500

    :loud_sound: Add some debug logging

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -9,14 +9,17 @@ const bugsnagClient = bugsnag(process.env.BUGSNAG_KEY);
 
 exports.handler = async function http(req) {
   const { pathParameters } = req;
+  console.log(pathParameters);
   if (pathParameters) {
     try {
       const { proxy: shortId } = pathParameters;
+      console.log('shortId: ', shortId);
       if (shortId && shortId.length === 8) {
         const shortLink = await data.get({
           table: 'links',
           key: shortId.replace('/', ''),
         });
+        console.log('shortLink: \n', shortLink);
         if (shortLink) {
           return helpers.returnShortLink(shortLink);
         }
